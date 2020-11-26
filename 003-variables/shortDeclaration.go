09b5a78c2791bd0295ffb0d3e1a8d318e27005fb
Mayistikar <andersonrodriguezce@gmail.com>
2020-11-26 13:17:19 -0500

    Variable declaration using short declaration

---
+++ 003-variables/shortDeclaration.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Short declaration operator :=
+	name := "Anderson"
+	fmt.Println(name)
+
+	// := Declare an asign a variable
+	lastName := "Rodriguez"
+	fmt.Println(lastName)
+
+	// = Reasign a variable's value
+	lastName = "Ceron"
+	fmt.Println(lastName)
+}

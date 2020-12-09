94a22bf3ab5a0ed07c7bd4bff16235bc4b121174
mayistikar <andersonrodriguezce@gmail.com>
2020-12-09 16:16:03 -0500

    Arrays examples

---
+++ 017-arrays/array.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Setting a integer array
+	var numbers [5]int
+	fmt.Println(numbers)
+
+	// Setting a strings array
+	var strings [4]string
+	fmt.Println(strings)
+
+	// Getting length
+	fmt.Println(len(numbers))
+}
